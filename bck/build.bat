3df351ef1ca4453ad83500cb0c2ad312ecdaf54a
gijswl <gijswlagerweij@gmail.com>
2017-05-24 17:28:38 +0200

    Update gitignore

--- bck/build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo Analyzing test/testbench.vhd test/ram.vhd and src/*.vhd
-ghdl -a --ieee=synopsys -fexplicit --workdir=simu --work=work test/testbench.vhd test/ram.vhd src/*.vhd
-echo Elaborating testbench
-ghdl -e --ieee=synopsys -fexplicit --workdir=simu --work=work testbench
-echo Running testbench
-ghdl -r --ieee=synopsys -fexplicit --workdir=simu --work=work testbench --vcd=results.vcd --stop-time=40us --ieee-asserts=disable
-pause
