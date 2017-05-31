24bc0c38804476ff8b49871983cb45be0e932c00
gijswl <gijswlagerweij@gmail.com>
2017-05-31 18:57:28 +0200

    Added CSRs in preparation for exceptions/interrupts

--- build.bat
+++ build.bat
@@ -4,5 +4,5 @@ ghdl -a --ieee=synopsys -fexplicit --workdir=simu --work=work test/testbench.vhd
 echo Elaborating testbench
 ghdl -e --ieee=synopsys -fexplicit --workdir=simu --work=work testbench
 echo Running testbench
-ghdl -r --ieee=synopsys -fexplicit --workdir=simu --work=work testbench --vcd=results.vcd --stop-time=40us --ieee-asserts=disable
+ghdl -r --ieee=synopsys -fexplicit --workdir=simu --work=work testbench --vcd=results.vcd --stop-time=40us --ieee-asserts=disable --stats
 pause
--- src/csrfile.vhd
+++ src/csrfile.vhd
@@ -20,9 +20,20 @@ architecture RTL of csrfile is
 	constant CSR_MARCHID   : std_logic_vector(31 downto 0) := X"00000000";
 	constant CSR_MIMPID    : std_logic_vector(31 downto 0) := X"00000000";
 	constant CSR_MHARTID   : std_logic_vector(31 downto 0) := X"00000000";
-	--
-	-- signal CSR_MISA    : std_logic_vector(31 downto 0) := X"40000100";
-	-- signal CSR_MSTATUS : std_logic_vector(31 downto 0);
+
+	signal CSR_MSTATUS    : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MISA       : std_logic_vector(31 downto 0) := X"40000100";
+	signal CSR_MEDELEG    : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MIDELEG    : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MIE        : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MTVEC      : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MCOUNTEREN : std_logic_vector(31 downto 0) := X"00000000";
+
+	signal CSR_MSCRATCH : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MEPC     : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MCAUSE   : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MTVAL    : std_logic_vector(31 downto 0) := X"00000000";
+	signal CSR_MIP      : std_logic_vector(31 downto 0) := X"00000000";
 
 	signal CSR_CYCLE   : std_logic_vector(63 downto 0) := X"0000000000000000";
 	signal CSR_TIME    : std_logic_vector(63 downto 0) := X"0000000000000000";
@@ -38,12 +49,27 @@ begin
 	end process;
 
 	with I_ADR select L_OUT <=
+		CSR_MSTATUS when X"300",
+		CSR_MISA when X"301",
+		CSR_MEDELEG when X"302",
+		CSR_MIDELEG when X"303",
+		CSR_MIE when X"304",
+		CSR_MTVEC when X"305",
+		CSR_MCOUNTEREN when X"306",
+		
+		CSR_MSCRATCH when X"340",
+		CSR_MEPC when X"341",
+		CSR_MCAUSE when X"342",
+		CSR_MTVAL when X"343",
+		CSR_MIP when X"344",
+
 		CSR_CYCLE(31 downto 0) when X"C00",
 		CSR_TIME(31 downto 0) when X"C01",
 		CSR_INSTRET(31 downto 0) when X"C02",
 		CSR_CYCLE(63 downto 32) when X"C80",
 		CSR_TIME(63 downto 32) when X"C81",
 		CSR_INSTRET(63 downto 32) when X"C82",
+		
 		CSR_MVENDORID when X"F11",
 		CSR_MARCHID when X"F12",
 		CSR_MIMPID when X"F13",
